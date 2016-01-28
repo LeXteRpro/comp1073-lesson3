0f1cade85e81a90bae2f7a559536ddc462a7e007
Orie <lexter@hotmail.ca>
2016-01-28 10:58:23 -0500

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,43 @@
+// "use strict";
+
+// var myNameString = "Tom";
+// var myAge = 30;
+// var myAgeString = "40";
+
+// console.log(myString + "'s App started...");
+// console.log(myString + " is " + myAge);
+
+// if(myAge S> 40) {
+//     console.log("You are old and wise!");
+//     console.log("This will always execute!");
+// }
+// else {
+//     console.log("You are young and strong");
+// }
+
+// if (myNameString === "Tom") {
+//     console.log("Hello Tom");
+// }
+
+// else if (myNameString === "Anju") {
+//     console.log("Hello Anju");
+// }
+
+
+"use strict";
+
+var username = "Tom";
+var age = 30;
+var isHungry = true;
+
+// New array
+var usernames = [];
+
+usernames[0] = "Mike";
+usernames[1] = "Rich";
+usernames[2] = "Anju";
+usernames[3] = "Margot";
+
+
+console.log(username[2]);
+
---
+++ backup/rough.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var myNameString = "Tom";
+var myAge = 30;
+var myAgeString = "40";
+
+console.log(myString + "'s App started...");
+console.log(myString + " is " + myAge);
+
+if(myAge S> 40) {
+    console.log("You are old and wise!");
+    console.log("This will always execute!");
+}
+else {
+    console.log("You are young and strong");
+}
+
+if (myNameString === "Tom") {
+    console.log("Hello Tom");
+}
+
+else if (myNameString === "Anju") {
+    console.log("Hello Anju");
+}
+
